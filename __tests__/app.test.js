c21ec658d6afd0de20f9be7e10e0b65ea8b7d033
unknown <tranvankhoa.1998@gmail.com>
2025-02-03 09:52:24 +0700

    fix testfile

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,16 @@
 const request = require('supertest');
-const app = require('../app'); // Đường dẫn đến file app.js của bạn
+const app = require('../app');
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = process.env.SECRET_KEY || 'mysecret';
+const token = jwt.sign({ id: 1, role: "admin" }, SECRET_KEY, { expiresIn: "1h" });
 
 describe("Todo API", () => {
     it("should return 200 for GET /todos", async () => {
-        const res = await request(app).get('/todos');
+        const res = await request(app)
+            .get('/todos')
+            .set('Authorization', `Bearer ${token}`); // Thêm token vào header
+
         expect(res.statusCode).toBe(200);
     });
 });
