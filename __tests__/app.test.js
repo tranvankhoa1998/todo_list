d7e4d3cfbbe33b27b8be83d9c565f78c2e48fd8d
unknown <tranvankhoa.1998@gmail.com>
2025-02-03 10:02:30 +0700

    feat expect testclose

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../app');
 const jwt = require('jsonwebtoken');
+const { pool } = require('../db'); // Import kết nối database
 
 const SECRET_KEY = process.env.SECRET_KEY || 'mysecret';
 const token = jwt.sign({ id: 1, role: "admin" }, SECRET_KEY, { expiresIn: "1h" });
