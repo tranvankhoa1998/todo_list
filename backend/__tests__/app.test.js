36fa7219ef1a32af063d4663e968c7f73eca207f
unknown <tranvankhoa.1998@gmail.com>
2025-02-03 14:48:31 +0700

    feat(backend/app1

--- backend/__tests__/app.test.js
+++ backend/__tests__/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../backend');
+const app = require('../backend/app');
 const jwt = require('jsonwebtoken');
 
 const SECRET_KEY = process.env.SECRET_KEY || 'mysecret';
